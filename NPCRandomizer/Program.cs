12a4f38332f170edc62959939395bc7b0c1d18fb
Ben Hall <bentomhall@gmail.com>
2018-02-24 14:53:04 -0500

    Add project files.

---
+++ NPCRandomizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NPCRandomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
